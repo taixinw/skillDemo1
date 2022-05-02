107ebfc0dd97f4d981721deff7193b96c22045f2
taixinw <twei@ucsd.edu>
2022-05-01 21:30:15 -0700

    Update skillDemoTest.java

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -3,9 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class skillDemoTest {
-
     @Test
     public void addition() {
-        assertEquals(2, skillDemo1.add(1,1));
+        assertEquals(2, skillDemo1.add(2,1));
     }
 }
